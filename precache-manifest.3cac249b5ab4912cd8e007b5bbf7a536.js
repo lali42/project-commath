2391d6d73e1426042cf411a3f3c92e59aa00decf
lali42 <lalita.su.61@ubu.ac.th>
2020-09-30 10:16:22 +0700

    Updates

---
+++ precache-manifest.3cac249b5ab4912cd8e007b5bbf7a536.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30afedf5f51f1d66c48a7dc71193ea2f",
+    "url": "/project-commath/index.html"
+  },
+  {
+    "revision": "fbb7719b032031370c82",
+    "url": "/project-commath/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2907373b67141dac7332",
+    "url": "/project-commath/static/js/2.bf167324.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-commath/static/js/2.bf167324.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbb7719b032031370c82",
+    "url": "/project-commath/static/js/main.57cc25ac.chunk.js"
+  },
+  {
+    "revision": "abdd1b03c71cf8421f7c",
+    "url": "/project-commath/static/js/runtime-main.21ad8a31.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/project-commath/static/media/logo.5d5d9eef.svg"
+  }
+]);
