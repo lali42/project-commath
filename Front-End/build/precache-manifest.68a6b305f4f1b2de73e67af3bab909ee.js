78f818b10649363afb65bc777865f0d1c20e7010
lali42 <lalita.su.61@ubu.ac.th>
2020-10-07 11:17:06 +0700

    backend and front-end

---
+++ Front-End/build/precache-manifest.68a6b305f4f1b2de73e67af3bab909ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fde777eea08a2463c6106d975323113e",
+    "url": "/project-commath/index.html"
+  },
+  {
+    "revision": "69ebbbfcd8dd5c77eeae",
+    "url": "/project-commath/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cd5f1ebd774dc3e1e698",
+    "url": "/project-commath/static/js/2.2f7531a1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/project-commath/static/js/2.2f7531a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69ebbbfcd8dd5c77eeae",
+    "url": "/project-commath/static/js/main.307b5432.chunk.js"
+  },
+  {
+    "revision": "abdd1b03c71cf8421f7c",
+    "url": "/project-commath/static/js/runtime-main.21ad8a31.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Home from './views/Home' ;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      
+      <Home/>
+
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +22,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+
+
     </div>
   );
 }
